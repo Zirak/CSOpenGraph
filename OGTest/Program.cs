e57cdbb26db70a70bf15522361de9320204d5ce3
Zirak <benstav@gmail.com>
2013-05-31 18:04:37 +0000

    initial; working

---
+++ OGTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenGraphParser;
+
+namespace OGTest {
+	class Program {
+		static void Main (string[] args) {
+			var data = new OpenGraph().ParseFromPath(@"..\..\test.html");
+
+			foreach (var pair in data) {
+				Console.WriteLine(@"{0} => {1}", pair.Key, pair.Value);
+			}
+		}
+	}
+}
---
+++ OpenGraph/OpenGraph.cs
@@ -0,0 +1,66 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+
+namespace OpenGraphParser {
+
+	public class OpenGraphData : Dictionary<string, string> { }
+
+	public class OpenGraph {
+		private string metaXPath = "//meta[contains(@property, \"og\")]";
+
+		public OpenGraphData ParseFromUrl (string url) {
+			var web = new HtmlWeb();
+			return this.ParseDocument(web.Load(url));
+		}
+
+		public OpenGraphData ParseFromPath (string path) {
+			var doc = new HtmlDocument();
+			doc.Load(path);
+			return this.ParseDocument(doc);
+		}
+
+		public OpenGraphData ParseDocument (HtmlDocument doc) {
+			var ret = new OpenGraphData();
+
+			foreach (var node in this.GetOGNodes(doc)) {
+				this.AddDataToResult(node, ret);
+			}
+
+			return ret;
+		}
+
+		private void AddDataToResult (HtmlNode node, OpenGraphData res) {
+			var key = ParseKeyFromNode(node);
+
+			if (!res.ContainsKey(key)) {
+				res.Add(key, ParseValueFromNode(node));
+			}
+		}
+
+		private string ParseKeyFromNode (HtmlNode node) {
+			return node.Attributes["property"].Value.Replace("og:", "");
+		}
+
+		private string ParseValueFromNode (HtmlNode node) {
+			return node.GetAttributeValue("content", string.Empty);
+		}
+
+		private IEnumerable<HtmlNode> GetOGNodes (HtmlDocument doc) {
+			var metas = doc.DocumentNode.SelectNodes(this.metaXPath);
+
+			foreach (var node in metas) {
+				if (this.IsValidOGNode(node)) {
+					yield return node;
+				}
+			}
+		}
+
+		private bool IsValidOGNode (HtmlNode node) {
+			if (!node.Attributes.Contains("property")) {
+				return false;
+			}
+
+			return node.Attributes["property"].Value.StartsWith("og:");
+		}
+	}
+}
