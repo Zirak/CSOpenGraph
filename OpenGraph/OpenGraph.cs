f9796e4a507950d6843eac384c0ec43fffe31520
Zirak <benstav@gmail.com>
2013-05-31 18:37:55 +0000

    added some documentation

--- OpenGraph/OpenGraph.cs
+++ OpenGraph/OpenGraph.cs
@@ -3,22 +3,45 @@ using System.Collections.Generic;
 
 namespace OpenGraphParser {
 
+	/// <summary>
+	/// A dummy class to hook up Open Graph data into.
+	/// </summary>
 	public class OpenGraphData : Dictionary<string, string> { }
 
+	/// <summary>
+	/// A simple OpenGraph data parser.
+	/// </summary>
 	public class OpenGraph {
 		private string metaXPath = "//meta[contains(@property, \"og\")]";
 
+		/// <summary>
+		/// Loads the page indicated by url, and parses the Open Graph
+		/// parameters inside.
+		/// </summary>
+		/// <param name="url">Url to webpage.</param>
+		/// <returns>OpenGraph data.</returns>
 		public OpenGraphData ParseFromUrl (string url) {
 			var web = new HtmlWeb();
 			return this.ParseDocument(web.Load(url));
 		}
 
+		/// <summary>
+		/// Opens the file denoted by path, and parses the Open Graph parameters
+		/// inside.
+		/// </summary>
+		/// <param name="path">Path to the file.</param>
+		/// <returns>OpenGraph data.</returns>
 		public OpenGraphData ParseFromPath (string path) {
 			var doc = new HtmlDocument();
 			doc.Load(path);
 			return this.ParseDocument(doc);
 		}
 
+		/// <summary>
+		/// Parses the Open Graph parameters inside the passed html document.
+		/// </summary>
+		/// <param name="doc">HtmlDocument (duh)</param>
+		/// <returns>OpenGraph data.</returns>
 		public OpenGraphData ParseDocument (HtmlDocument doc) {
 			var ret = new OpenGraphData();
 
@@ -29,6 +52,11 @@ namespace OpenGraphParser {
 			return ret;
 		}
 
+		/// <summary>
+		/// Parses the key/value pair from a node, and adds them to the result.
+		/// </summary>
+		/// <param name="node">Node to get info from.</param>
+		/// <param name="res">Result object to extract data to.</param>
 		private void AddDataToResult (HtmlNode node, OpenGraphData res) {
 			var key = ParseKeyFromNode(node);
 
